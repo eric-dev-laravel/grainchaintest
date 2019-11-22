dde207b42994f102bd562fa88b1704893f7bcb79
Eric <eric.vargas@hallmg.com>
2019-11-21 20:28:06 -0600

    First commit

---
+++ grainchaintest/grainchaintest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grainchaintest
+//
+//  Created by ADMINISTRADOR on 21/11/19.
+//  Copyright © 2019 grainchain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
